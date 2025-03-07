417fff6aa6af61ed832b24b6074a218172d020ff
Alexandre <alexandresantosfonseca47@gmail.com>
2025-03-06 21:02:10 -0300

    Delete PW2/4_include_required/assets directory

--- PW2/4_include_required/assets/mexico.php
+++
@@ -1,3 +0,0 @@
-<?php include './header.php';  ?>
-<h1>Hello Mexico</h1>
-<?php include './footer.php';  ?>
--- PW2/4_include_required/assets/peru.php
+++
@@ -1,3 +0,0 @@
-<?php include './header.php';  ?>
-<h1>Hello Peru</h1>
-<?php include './footer.php';  ?>
