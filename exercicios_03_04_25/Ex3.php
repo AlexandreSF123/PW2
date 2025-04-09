e98e6877980de4ff06b7963e6d38018d057e6872
Alexandre <alexandresantosfonseca47@gmail.com>
2025-04-09 02:32:43 -0300

    Update Ex3.php

--- exercicios_03_04_25/Ex3.php
+++ exercicios_03_04_25/Ex3.php
@@ -1,32 +1,34 @@
 <?php
-// tentei com mais um forEach mas só esse deu
-$Escola = [
-    "Etec" => [
-        "Desenvolvimento de Sistemas" => [
-            "1-Programação Mobile" => ["   Módulo I", "   Módulo II"],
-            "2-Programação Web" => ["   Módulo I", "   Módulo II", "   Módulo III", "   Módulo IV"],
-            "3-Técnicas Avançadas de Programação de Algoritmos" => [],
-            "4-Inglês" => ["Módulo I","   Módulo II", "Módulo III"]
-        ],
-        "Administração" => [
-            "1-Recursos Humanos" => ["Módulo I", "Módulo II"],
-            "2-Gestão de Pessoas" => [],
-            "3-Teoria da Administração" => [],
-            "4-Inglês" => [" Módulo I"]
-        ]
+$cursos = [
+    "Etec" => [ 
+    "Desenvolvimento de Sistemas" => [
+        "Programação Mobile" => ["Módulo I", "Módulo II"],
+        "Programação Web" => ["Módulo I", "Módulo II", "Módulo III", "Módulo IV"],
+        "Técnicas Avançadas de Programação de Algoritmos" => [],
+        "Inglês" => ["Módulo I", "Módulo II", "Módulo III"]
+    ],
+    
+    "Administração" => [
+        "Recursos Humanos" => ["Módulo I", "Módulo II"],
+        "Gestão de Pessoas" => [],
+        "Teoria da Administração" => [],
+        "Inglês" => ["Módulo I"]
+    ],
+    
     ]
 ];
-function displayArray($array) {
-    foreach ($array as $key => $value) {
-        if (is_array($value)) {
-            echo "<li>" ."<b>"."$key\n" . "</b>" . "</li>";
-            echo "<br>";
-            displayArray($value);
-        } else {
-            echo " - $value\n";
-            echo "<br>";
+
+foreach ($cursos as $instituicao => $areas) {
+    echo "<li><strong>$instituicao</strong></li><br>";
+    foreach ($areas as $area => $subcategorias) {
+        echo "<ul></ul><li style='color: blue;'>$area</li><br>" ; 
+    foreach ($subcategorias as $subcategoria => $modulos) {
+        echo "<ul><li>$subcategoria</li></ul>";
+        foreach ($modulos as $modulo) {
+            echo "<ul><dd><li>$modulo </li></dd></ul>";
         }
     }
+  }
 }
 
-displayArray($Escola);
+?>
